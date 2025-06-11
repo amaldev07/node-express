3ce686eae33cceee5ea0326185fa04f5ea4e3aec
Amaldev <amaldev.psn@gmail.com>
2025-06-12 01:07:59 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app = express();
+app.use(express.json());
+// const session = require('express-session');
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.get('/testApi', function (req, res) {
-  res.send({name : 'dev ssss'});
+  res.send({ name: 'dev ssss' });
+});
+
+app.post('/api/receive-tin', (req, res) => {
+  console.log("HIIII");
+  const tin = req.body.tin;
+  console.log("HIIII"+tin); // Log 123
+  // Option 1: Store in session/cookie
+  // Option 2: Store in DB and generate a one-time token (for extra security)
+  // Now, redirect to the Angular app
+  res.redirect('http://localhost:4200/home');
 });
 
 app.listen(3000, function () {
